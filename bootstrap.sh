85aa871d5672d4dc054a6ecdd05f9d32c17a2ccb
ZCode <zcode.ia@gmail.com>
2025-09-14 16:38:13 -0300

    chore: use tools folder as submodule

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-if [[ "$SKIP" == "enable_pre_commit" ]]; then
-    echo "Skipping pre-commit.."
+# Bash safety flags:
+# -e: exit on any error
+# -u: treat unset variables as an error and exit
+# -o pipefail: fail if any command in a pipeline fails
+set -euo pipefail
+
+WORKSPACE_DIR=$(git rev-parse --show-toplevel)
+BOOTSTRAP_DIR="${WORKSPACE_DIR}/tools/bootstrap"
+
+SKIP=${SKIP:-""}
+
+if [[ "$SKIP" =~ "init_git_submodule" ]]; then
+    echo "Skipping git submodule init.."
 else
-    bash tools/enable_pre_commit.sh
+    bash "${BOOTSTRAP_DIR}"/init_git_submodule.sh
 fi
 
-if [[ "$SKIP" == "install_npm_dependencies" ]]; then
+if [[ "$SKIP" =~ "enable_pre_commit" ]]; then
+    echo "Skipping pre-commit.."
+else
+    bash "${BOOTSTRAP_DIR}"/enable_pre_commit.sh
+fi
+
+if [[ "$SKIP" =~ "install_npm_dependencies" ]]; then
     echo "Skipping install-npm-dependencies.."
 else
-    bash tools/install_npm_dependencies.sh
+    bash "${BOOTSTRAP_DIR}"/install_npm_dependencies.sh
 fi
